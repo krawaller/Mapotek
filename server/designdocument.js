0ff8579381f06efce99733e452d3a95d13668b17
KungD <david@krawaller.se>
2011-02-05 10:13:55 +0100

    status view in design document

--- server/designdocument.js
+++ server/designdocument.js
@@ -3,6 +3,28 @@
    "views": {
        "apotek": {
            "map": "function(doc){ if(doc.apotek){ emit(doc.apotek, doc); } }"
+       },
+       "status": {
+	       "map": "function(doc){ if(doc.chain){ emit(doc.chain, doc); } }",
+	       "reduce": "function(keys,values){ var ret= {}; values.forEach(function(a){ if (!ret[a.chain]){ ret[a.chain] = { number: 0, witherrors: 0};} ret[a.chain].number++; if(a.errors && a.errors.length){ret[a.chain].witherrors++;} }); return ret; }"
        }
    }
+}
+
+
+function(keys,values){
+	var ret = {};
+	values.forEach(function(a){
+		if (!ret[a.chain]){
+			ret[a.chain] = {
+				number: 0,
+				witherrors: 0
+			};
+		}
+		ret[a.chain].number++;
+		if(a.errors && a.errors.length){
+			ret[a.chain].witherrors++;
+		}
+	});
+	return ret;
 }
