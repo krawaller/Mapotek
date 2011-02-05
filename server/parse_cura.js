52dc5950c6ae3c412b48f6dad9614562a990c544
KungD <david@krawaller.se>
2011-02-05 19:38:13 +0100

    better cura parsing, plus a few more manual coords

--- server/parse_cura.js
+++ server/parse_cura.js
@@ -3,7 +3,9 @@ Cura dårå! Observera att jag vill parsa det individuella apotekets KONTAKTSIDA
 Verkar som att de allihop har samma öppettider alla dagar? Hmm.
 Kontaktinfolistan innehåller ibland fax, ibland inte. Careful!
 
-OBS! Ingen jävla geoinfo! No coords! :(
+OBS! Ingen jävla geoinfo! No coords! :( Hämtar manuellt.
+	
+Observera att apotekets NAMN är lite inkonsekvent. Hämtar det från rubriken uppe till vänster på kontaktsidan!
 */
 
 var scraper = require('./scraper'),
@@ -32,7 +34,7 @@ db.view('/mapotek/_design/v1.0/_view/apotek', { key: "Cura" }, function(err, doc
 	doc.rows.forEach(function(row, i){
 		if(i != 0){ return; } // just one for now
 		var obj = row.value,
-			url =  'http://localhost/mapotek/server/tmp/parse_cura_6.html'; // should be obj.href
+			url =  'http://localhost/mapotek/server/tmp/parse_cura.html'; // should be obj.href
 		
 		scraper(url, function(err, $) {
 		    if (err) {throw err;}
@@ -43,7 +45,13 @@ db.view('/mapotek/_design/v1.0/_view/apotek', { key: "Cura" }, function(err, doc
 				"Gävle": ["60.6479","17.1488"],
 				"Göteborg": ["57.682","12.0023"],
 				"Flygstaden Halmstad": ["56.6864","12.7868"],
-				"Högskolan Halmstad": ["56.6648","12.8779"]
+				"Högskolan, Halmstad": ["56.6648","12.8779"],
+				"Haninge": ["59.1765","18.1617"],
+				"Helsingborg": ["56.0643","12.719"],
+				"Häggvik": ["59.438","17.9316"],
+				"Jönköping": ["57.7611","14.1678"],
+				"Hässleholm": ["56.164264","13.779902"],
+				"Högsbo": ["57.6509","11.9455"]
 				// TODO - resten! :P
 			};
 			var errors = [], ret,
@@ -57,12 +65,18 @@ db.view('/mapotek/_design/v1.0/_view/apotek', { key: "Cura" }, function(err, doc
 			if ((globalopen.length !== 5) || (globalclose.length !== 5)){
 				errors.push("Strange times! "+globalopen+" "+globalclose);
 			}
-			var name = getMatch("name",$("div.textarea > div.text").text(),/apotekschef.*?Cura apoteket,?\w*(.*)$/,errors).replace(/^\s*/,"");
+			// try to get name from headline in upper left
+			var name = $("p.logo-suffix").text().replace(/Cura apoteket,?\w*/,"").replace(/^\s*/,"");
+			// plan B if that failed
+			if (!name){
+				name = getMatch("name",$("div.textarea > div.text").text(),/apotekschef.*?Cura apoteket,?\w*(.*)$/,[]).replace(/^\s*/,"");
+			}
+			ensureOkString("name",name,errors);
 			ret = {
 				name: name,
 				chain: "Cura",
 				address: {
-					street: getMatch("street",text,/Adress(.*?)\d{3} \d{2} (.*?)E-post/,errors).replace(/,\s*$/g,""),
+					street: getMatch("street",text,/Adress:?\s*(.*?)\d{3} \d{2}(.*?)E-post/,errors).replace(/,\s*$/g,""),
 					zipcode: getMatch("zipcode",text,/(\d{3} \d{2}) (.*?)E-post/,errors).replace(/\D/g,""),
 					city: getMatch("city",text,/(\D*?)E-post/,errors)
 				},
